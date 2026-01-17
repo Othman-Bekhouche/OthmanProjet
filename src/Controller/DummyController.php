6eb4011ad81a4952834839af0998faafffb03a8d
Othman Bekhouche <bekhouche.othman@gmail.com>
2026-01-17 16:38:58 +0100

    Update Twig

--- src/Controller/DummyController.php
+++ src/Controller/DummyController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class DummyController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(): Response
